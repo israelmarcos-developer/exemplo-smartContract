3f974f4ef431949f71f76ea7de07b600d751bfb4
IsraelMarcos_DeveLoper <israelmarcos.developer@gmail.com>
2023-03-02 22:19:59 -0300

    att-student

--- contracts/contractTest.sol
+++ contracts/contractTest.sol
@@ -1,9 +1,32 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.7;
 
-contract contractTest {
-  uint public meuNumero = 1;
-  function setInt(uint _novoNumero) public {
-    meuNumero = _novoNumero;
-  }
+contract ContractTest {
+    uint public cod_licitacao;
+    string public objeto;
+    uint public data;
+    string public orgao;
+
+    function setAttributes(uint _cod_licitacao, string memory _objeto, uint _data, string memory _orgao) public {
+        cod_licitacao = _cod_licitacao;
+        objeto = _objeto;
+        data = _data;
+        orgao = _orgao;
+    }
+
+    function getCodLicitacao() public view returns (uint) {
+        return cod_licitacao;
+    }
+
+    function getObjeto() public view returns (string memory) {
+        return objeto;
+    }
+
+    function getData() public view returns (uint) {
+        return data;
+    }
+
+    function getOrgao() public view returns (string memory) {
+        return orgao;
+    }
 }
